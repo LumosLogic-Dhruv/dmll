2c16cdcac53d335da4b4425e853311300e579b1e
Dhruv <sheredhruv@gmail.com>
2026-02-12 18:28:12 +0530

    feat: remove scroll indicator from Hero component

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -165,26 +165,6 @@ const Hero = () => {
             />
           </motion.div>
         </div>
-
-        {/* Scroll Indicator */}
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1.5 }}
-          className="absolute bottom-10 left-1/2 -translate-x-1/2"
-        >
-          <motion.div
-            animate={{ y: [0, 8, 0] }}
-            transition={{ duration: 2, repeat: Infinity, ease: "easeInOut" }}
-            className="w-6 h-10 border border-border rounded-full flex items-start justify-center p-2"
-          >
-            <motion.div
-              animate={{ y: [0, 12, 0] }}
-              transition={{ duration: 2, repeat: Infinity, ease: "easeInOut" }}
-              className="w-1 h-2 bg-foreground rounded-full"
-            />
-          </motion.div>
-        </motion.div>
       </div>
 
       {/* Bottom Gradient Fade */}
