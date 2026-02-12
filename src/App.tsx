4e144e2ea932100abe8c7a39575113317770c67f
Dhruv <sheredhruv@gmail.com>
2026-02-12 14:14:52 +0530

    feat: implement ScrollToTop component and update routing in App fix: update Firebase project configuration

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import ScrollToTop from "@/components/ScrollToTop";
 
 // Main Pages
 import Index from "./pages/Index";
@@ -38,6 +39,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ScrollToTop />
           <Routes>
             {/* Main Pages */}
             <Route path="/" element={<Index />} />
